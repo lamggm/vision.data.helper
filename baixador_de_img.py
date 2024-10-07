e2dc6b7ec95fb6e968ab5631080cbba933bc7dc4
lamggm <150146192+lamggm@users.noreply.github.com>
2024-10-07 09:50:43 -0300

    Update baixador_de_img.py

--- baixador_de_img.py
+++ baixador_de_img.py
@@ -3,24 +3,23 @@ import requests
 from serpapi import GoogleSearch
 
 # Função para baixar imagens usando a API SerpApi
-def download_images(query, total_count=400, output_directory=r'C:\Users\lamgg\Documents\GitHub\vision\Treinamento\dataset'):
-    api_key = "16fa11d056bfd2e3ea6158750fa0e425f8d0564d77d461bbb68accd8eb9b791b"  # Insira sua chave da API SerpApi
-    images_per_request = 100  # SerpApi permite até 100 imagens por requisição
+def download_images(query, total_count=400, output_directory=r'**********************YOUR DIR HERE**********************'):
+    api_key = "**********************YOUR KEY HERE**********************"
+    images_per_request = 100
     downloaded = 0
     start = 0
 
-    # Verifica se o diretório de saída existe, caso contrário, cria
     if not os.path.exists(output_directory):
         os.makedirs(output_directory)
 
     while downloaded < total_count:
         params = {
-            "engine": "google",       # Motor de busca
-            "q": query,               # Termo de busca
-            "tbm": "isch",            # Tipo de busca: imagens
-            "num": images_per_request, # Número de imagens por requisição (máx. 100)
-            "start": start,            # Ponto de partida da próxima requisição
-            "api_key": api_key        # Chave da API SerpApi
+            "engine": "google",
+            "q": query,
+            "tbm": "isch", 
+            "num": images_per_request,
+            "start": start, 
+            "api_key": api_key
         }
 
         search = GoogleSearch(params)
@@ -36,7 +35,6 @@ def download_images(query, total_count=400, output_directory=r'C:\Users\lamgg\Do
                 img_url = image['original']
                 img_data = requests.get(img_url).content
                 
-                # Nome do arquivo baseado no contador (de 1 até o total desejado)
                 img_name = os.path.join(output_directory, f"{downloaded + 1}.jpg")
                 
                 with open(img_name, "wb") as img_file:
@@ -50,8 +48,6 @@ def download_images(query, total_count=400, output_directory=r'C:\Users\lamgg\Do
             except Exception as e:
                 print(f"Erro ao baixar imagem {img_url}: {e}")
 
-        # Atualiza o ponto de partida para a próxima requisição
         start += images_per_request
 
-# Baixando até 400 imagens de parafusos
-download_images("scratch marks on a cabinet", total_count=100)
+download_images("**********************YOUR SEARCH HERE**********************", total_count=100)
